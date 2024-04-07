b37be4e8fc5f1fb1e673ef90f6ef6ebf78bf4942
Abdallah Alsamman <sammanabdallah@gmail.com>
2024-04-08 02:09:05 +0300

    Update CameraPreview.java

--- hiddencamera/src/main/java/com/androidhiddencamera/CameraPreview.java
+++ hiddencamera/src/main/java/com/androidhiddencamera/CameraPreview.java
@@ -243,8 +243,8 @@ class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
 
                             try {
                                 mCamera.startPreview();
-                            }  catch (RuntimeException e) {
-                                throw new RuntimeException(e);
+                            }  catch (Exception e) {
+                                Log.e("CameraPreview", "Failed to startPreview");
                             }
                             safeToTakePicture = true;
                         }
